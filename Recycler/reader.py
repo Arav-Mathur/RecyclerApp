80a7e021522bc965963b150790cc749d46b2d006
Arav-Mathur <aravmathur007@gmail.com>
2024-01-04 20:55:33 -0500

    reading csv

---
+++ Recycler/reader.py
@@ -0,0 +1,27 @@
+import csv
+rows = []
+with open("data.csv","r") as f:
+  csvreader = csv.reader(f)
+  for row in csvreader:
+    rows.append(row)
+headers = rows[0]
+recycleData = rows[1:]
+print(headers)
+
+tempRecycle = list(recycleData)
+headers.pop(0)
+headers.pop(3)
+headers.pop(headers.index("Drop Off Instructions"))
+for item in recycleData:
+    item[item.index('')] = 'nan'
+    item.pop(0)
+    item.pop(3)
+    try:
+        item.pop(5)
+    except IndexError as e:
+       print(e)
+       continue
+    continue
+for item in recycleData:
+    if len(item) > 5:
+       print("aw shucks")
