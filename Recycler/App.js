53515fb1c8d486dd1fb2e47abfb76fc52a751df2
12Firestar <anvaymathur007@gmail.com>
2024-01-05 18:12:10 -0500

    try

--- Recycler/App.js
+++ Recycler/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import myStack from './navigation/stack'
-import SwitchComponent from './screens/searchscreen'
+import search from './screens/searchscreen'
+
 export default function App() {
   //return <myStack/>;
-  return <SwitchComponent/>
+  var search1 = new search
 }
 
 const styles = StyleSheet.create({
