a72fcab0f34f4ce234933f4992f07dc05b288223
12Firestar <anvaymathur007@gmail.com>
2024-01-07 17:30:21 -0500

    daa

--- Recycler/App.js
+++ Recycler/App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import myStack from './navigation/stack'
 import Search from './screens/searchscreen'
 
 
