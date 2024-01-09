109ccfdbbdab8c20c3db4808b080dac42c51fe4b
Arav-Mathur <aravmathur007@gmail.com>
2024-01-08 20:09:02 -0500

    using synonyms

--- Recycler/readerv3.py
+++ Recycler/readerv3.py
@@ -1,27 +1,31 @@
 import csv
 import pandas as pd 
+import math
 class Reader:
     def __init__(self):
-        sheetdata = pd.read_csv("data.csv")
-        # name = data['Material Name'].tolist()
-        # print(name)
-        self.data = sheetdata.drop(['Material Name','Stream Name','Drop Off Instructions'],axis=1)
+        self.sheetdata = pd.read_csv("data.csv")
+        self.data = self.sheetdata.drop(['Material Name','Stream Name','Drop Off Instructions'],axis=1)
         self.searchvalue = ''
-        #print(data)
     def find(self, searchval):
-        # mystring = 'Dandelion' 
-        # get input from searchbar as search value
-
-        # result = data['Material Title'].where(data['Material Title'] == mystring.capitalize())
-        # result = data[['Material Title'] == mystring]
         try:
-            self.searchvalue = str(searchval).capitalize()
+            self.searchvalue = str(searchval)
             result = self.data.loc[self.data['Material Title'] == self.searchvalue]
-            print(result['Stream Title'][0])
-        except KeyError as e:
-            print(e)
-        # data['Material Title'].where(searchval)
-        # data['Material Synonym'][index].split(",")
-        # or data['Material Synonym'].where(searchval) --> do it as a for loop instead of .where function to use split
+            print(result['Stream Title'].values[0])
+        except:
+            #print("Sorry we could not find your item in the database, please look at your local website for more information!")
+            # print(e)
+            # return 0
+            synonyms = pd.DataFrame(columns = ['Material Title','Material Synonyms'])
+            #print(synonyms)
+            mysyn = self.data['Material Synonyms'].tolist()
+            #print(mysyn)
+            for i in range(len(mysyn)):
+                if not(pd.isnull(mysyn[i])):
+                    #synonyms.add(mysyn[i].split(","))
+                    #print(mysyn[i].split(","))
+                    synonyms = synonyms.concat({'Material Title':self.data['Material Title'][i],'Material Synonyms':mysyn[i].split(",")})
+            print(synonyms)
+            # or data['Material Synonym'].where(searchval) --> do it as a for loop instead of .where function to use split
 r1 = Reader()
-r1.find('dandelion')
+#r1.find(r1.sheetdata['Material Title'][144])
+r1.find("amongus")
