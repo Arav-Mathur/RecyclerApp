58d44c892e9783537f4fc0381669c1be239e837c
Arav-Mathur <aravmathur007@gmail.com>
2024-07-13 20:25:39 -0400

    not sure what changed but ya

--- Recycler/App.js
+++ Recycler/App.js
@@ -15,7 +15,7 @@ import Search from './screens/searchscreen'
     });};
   handleSearch = async () => {
     console.log('Sending request to Flask server...');
-    const response = await fetch('http://192.168.68.59:5000/find', {
+    const response = await fetch('http://10.210.1.129:5000/find', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ search_val: this.state.search }),
@@ -33,8 +33,7 @@ import Search from './screens/searchscreen'
           <Search onSearchChange={this.handleSearchChange}/>
         </SafeAreaView>
         <Text style={styles.text}>{"You may dispose this at:"}</Text>
-        <Text style={styles.text}>{this.state.result.length>0? this.state.result.join(', '):
-        "Sorry, no results were found, please navigate to your local website to get more information"}</Text>
+        <Text style={styles.text}>{this.state.result.length>0? this.state.result.join(', '):"Sorry, no results were found, please navigate to your local website to get more information"}</Text>
         <Text style={styles.description}>Recycler App: This project is all about making recycling easy for everyone in our community. The Recycler App is like a friendly guide, helping you find the best ways to recycle different things. It's simple to use and gives you practical tips on how to be kind to the environment. Let's all work together for a cleaner and more eco-friendly future, one recyclable at a time!</Text>
         <View style={styles.img}>
           <Image source={require('./assets/image1.png')}style={styles.img}/>
